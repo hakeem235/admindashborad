76469dc6776a160489dad8bfbd64c51176d09dee
hakeem235 <hakeemkoo@gmail.com>
2022-05-30 12:25:06 -0400

    update

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from '../../components/navbar/Navbar'
+import "./home.scss"
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <Sidebar/>
+      <div className="honeContainer">
+      <Navbar/>
+      home container
+      </div>
+     
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/new/New.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./new.scss"
+
+const New = () => {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  )
+}
+
+export default New
